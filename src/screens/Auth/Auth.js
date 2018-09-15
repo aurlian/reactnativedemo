a6b56f918b596f9e2407c82c58bf91e1fbe9dc30
Mher <mher@codeparticle.com>
2018-09-15 15:07:45 -0700

    login page

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  ImageBackground
+} from "react-native";
 import startMainTabs from "../MainTabs/startMainTabs";
+import DefaultTextInput from "../../components/UI/DefaultTextInput/DefaultTextInput";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
+import MainText from "../../components/UI/MainText/MainText";
+import sunsetImage from "../../assets/sunset.jpg";
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -8,12 +19,48 @@ class AuthScreen extends Component {
   };
   render() {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler} />
-      </View>
+      <ImageBackground source={sunsetImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Please Login</HeadingText>
+          </MainText>
+
+          <Button title="Switch to Login" onPress={this.loginHandler} />
+          <View style={styles.inputContainer}>
+            <DefaultTextInput
+              placeholder="Your Email Address"
+              style={styles.input}
+            />
+            <DefaultTextInput placeholder="Password" style={styles.input} />
+            <DefaultTextInput
+              placeholder="Confirm Password"
+              style={styles.input}
+            />
+          </View>
+          <Button title="Submit" onPress={this.loginHandler} />
+        </View>
+      </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  backgroundImage: {
+    width: "100%",
+    flex: 1
+  },
+  input: {
+    backgroundColor: "#eee",
+    borderColor: "#bbb"
+  }
+});
 
 export default AuthScreen;
